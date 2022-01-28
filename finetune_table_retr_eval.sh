aea676a825c76873fa3dc8787fb8136ef3f3bc1e
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-01-28 13:46:07 -0600

    add batch eval

--- finetune_table_retr_eval.sh
+++ finetune_table_retr_eval.sh
@@ -1,10 +1,11 @@
+step=$1
 python ./finetune_table_retr.py \
     --model_path ./pretrained_models/tqa_reader_base \
-    --fusion_retr_model ./output/finetune_table_retr_margin/epoc_0_step_24000_model.pt \
-    --eval_data ./data/retrieved_test.json \
+    --fusion_retr_model ./output/finetune_table_retr_margin/epoc_0_step_${step}_model.pt \
+    --eval_data ./data/auto_tune/sql_retr_data_dev.jsonl \
     --n_context 100 \
     --per_gpu_batch_size 1 \
     --cuda 0 \
-    --name retrieved_test_margin \
+    --name auto_tune_dev_${step} \
     --checkpoint_dir output \
 
