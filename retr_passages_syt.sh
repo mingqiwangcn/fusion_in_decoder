c02744e31e37a4d8bad3779bafe89f9ffcff80e6
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-02-09 15:29:50 -0600

    update passage file

--- retr_passages_syt.sh
+++ retr_passages_syt.sh
@@ -5,14 +5,14 @@ fi
 
 dataset=$1
 expr=$2
-passage_file=$3
+file_name=$3
 sql_data_dir=~/code/open_table_discovery/table2question/dataset/${dataset}/sql_all_per_10
 out_dir=${sql_data_dir}/${expr}
 
 python ./passage_ondisk_retrieval.py \
     --model_path ./pretrained_models/tqa_retriever \
     --index_file ./data/on_disk_index_${dataset}_${expr}/populated.index \
-    --passage_file ~/code/open_table_discovery/table2txt/dataset/${dataset}/${expr}/${passage_file} \
+    --passage_file ./data/on_disk_index_${dataset}_${expr}/${file_name} \
     --data ${sql_data_dir}/fusion_query.jsonl \
     --output_path ${out_dir}/retrieved_data.jsonl \
     --n-docs 100 \
