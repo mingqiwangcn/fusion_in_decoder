58f72368d189203b4c79894bef7f2b23568915b5
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-02-15 10:41:41 -0600

    fix data dir

--- finetune_syt_retr_eval_bm25.sh
+++ finetune_syt_retr_eval_bm25.sh
@@ -7,11 +7,11 @@ mode=$2
 expr=$3
 epoch=$4
 step=$5
-data_dir=~/code/open_table_discovery/table2txt/dataset/${dataset}/${expr}
+data_dir=~/code/open_table_discovery/table2txt/dataset/${dataset}/bm25_${expr}/${mode}
 python ./finetune_table_retr.py \
     --model_path ./pretrained_models/tqa_reader_base \
     --fusion_retr_model ./output/bm25_train_syt_${dataset}_${expr}/epoc_${epoch}_step_${step}_model.pt \
-    --eval_data ${data_dir}/fusion_retrieved_${mode}.json \
+    --eval_data ${data_dir}/bm25_fusion_retrieve_${mode}.jsonl \
     --n_context 100 \
     --per_gpu_batch_size 1 \
     --cuda 0 \
