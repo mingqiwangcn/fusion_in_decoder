7b3d5ce8a91a845d24631740344120645c165061
Chameleon Cloud User <cc@expr-0.novalocal>
2022-03-18 18:10:46 -0500

    update syt dev script

--- finetune_syt_retr_eval.sh
+++ finetune_syt_retr_eval.sh
@@ -10,11 +10,10 @@ step=$5
 data_dir=~/code/open_table_discovery/table2txt/dataset/${dataset}/${expr}
 python ./finetune_table_retr.py \
     --model_path ./pretrained_models/tqa_reader_base \
-    --fusion_retr_model ./output/train_syt_${dataset}_${expr}/epoc_${epoch}_step_${step}_model.pt \
-    --eval_data ${data_dir}/fusion_retrieved_${mode}.json \
+    --fusion_retr_model ./pretrained_models/train_syt_${dataset}_${expr}/epoc_${epoch}_step_${step}_model.pt \
+    --eval_data ${data_dir}/fusion_retrieved_${mode}.jsonl \
     --n_context 100 \
     --per_gpu_batch_size 1 \
     --cuda 0 \
     --name syt_${mode}_${dataset}_${expr}_step_${step} \
     --checkpoint_dir output \
-    --retr_model_type general \
