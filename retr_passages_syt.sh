04585d411e22284f4a2f997b7d5e815768be8524
Chameleon Cloud User <cc@expr-1.novalocal>
2022-03-19 11:50:02 -0500

    update file name

--- retr_passages_syt.sh
+++ retr_passages_syt.sh
@@ -1,10 +1,11 @@
-if [ "$#" -ne 2 ]; then
-  echo "Usage: ./retr_passages_syt.sh <dataset> <experiment>"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: ./retr_passages_syt.sh <dataset> <experiment> <part>"
   exit
 fi
 
 dataset=$1
 expr=$2
+part=$3
 file_name=passages.jsonl
 sql_data_dir=~/code/open_table_discovery/table2question/dataset/${dataset}/auto_sql
 out_dir=${sql_data_dir}/${expr}
@@ -13,6 +14,6 @@ python ./passage_ondisk_retrieval.py \
     --model_path ./pretrained_models/tqa_retriever \
     --index_file ./data/on_disk_index_${dataset}_${expr}/populated.index \
     --passage_file ./data/on_disk_index_${dataset}_${expr}/${file_name} \
-    --data ${sql_data_dir}/fusion_query.jsonl \
-    --output_path ${out_dir}/fusion_retrieved.jsonl \
+    --data ${sql_data_dir}/fusion_query_parts/fusion_query_${part}.jsonl \
+    --output_path ${out_dir}/fusion_retrieved_${part}.jsonl \
     --n-docs 500 \
