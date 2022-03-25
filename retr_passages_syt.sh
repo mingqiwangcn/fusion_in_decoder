b4c7d336a669467cb5131b644a557738902c268b
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-03-24 23:53:27 -0500

    update syt retr

--- retr_passages_syt.sh
+++ retr_passages_syt.sh
@@ -1,12 +1,12 @@
 if [ "$#" -ne 3 ]; then
-  echo "Usage: ./retr_passages_syt.sh <dataset> <experiment> <part>"
+  echo "Usage: ./retr_passages_syt.sh <dataset> <experiment> <mode>"
   exit
 fi
 
 dataset=$1
 expr=$2
-part=$3
-file_name=passages.jsonl
+mode=$3
+file_name=token_text.jsonl
 sql_data_dir=~/code/open_table_discovery/table2question/dataset/${dataset}/auto_sql
 out_dir=${sql_data_dir}/${expr}
 
@@ -14,6 +14,6 @@ python ./passage_ondisk_retrieval.py \
     --model_path ./pretrained_models/tqa_retriever \
     --index_file ./data/on_disk_index_${dataset}_${expr}/populated.index \
     --passage_file ./data/on_disk_index_${dataset}_${expr}/${file_name} \
-    --data ${sql_data_dir}/fusion_query_parts/fusion_query_${part}.jsonl \
-    --output_path ${out_dir}/fusion_retrieved_${part}.jsonl \
+    --data ${sql_data_dir}/fusion_query_${mode}.jsonl \
+    --output_path ${out_dir}/fusion_retrieved_${mode}.jsonl \
     --n-docs 100 \
