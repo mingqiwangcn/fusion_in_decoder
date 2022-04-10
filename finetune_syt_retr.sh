48993755cadf48c0c840ce7236e48f99185d6398
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-04-10 12:10:51 -0500

    update out dir

--- finetune_syt_retr.sh
+++ finetune_syt_retr.sh
@@ -6,7 +6,7 @@ dataset=$1
 exprt=$2
 sql_expr=$3
 exprt_dir=/home/cc/code/open_table_discovery/table2question/dataset/${dataset}/${sql_expr}/${exprt}
-chk_name=train_syt_${dataset}_${exprt} 
+chk_name=train_syt_${dataset}_${exprt}_${sql_expr} 
 python ./finetune_table_retr.py \
     --do_train \
     --model_path ./pretrained_models/tqa_reader_base \
