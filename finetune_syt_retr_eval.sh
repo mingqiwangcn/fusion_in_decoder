8a5bff47bbcceb1c66e8f1b473ef3a5869333ed3
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-04-12 11:33:57 -0500

    update out dir

--- finetune_syt_retr_eval.sh
+++ finetune_syt_retr_eval.sh
@@ -1,19 +1,20 @@
-if [ "$#" -ne 5 ]; then
-    echo "Usage: ./finetune_syt_retr_eval.sh <dataset> <mode> <expr> <epoch> <step>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: ./finetune_syt_retr_eval.sh <dataset> <mode> <expr> <sql_expr> <epoch> <step>"
     exit
 fi
 dataset=$1
 mode=$2
 expr=$3
-epoch=$4
-step=$5
+sql_expr=$4
+epoch=$5
+step=$6
 data_dir=~/code/open_table_discovery/table2txt/dataset/${dataset}/${expr}
 python ./finetune_table_retr.py \
     --model_path ./pretrained_models/tqa_reader_base \
-    --fusion_retr_model ./output/train_syt_${dataset}_${expr}/epoc_${epoch}_step_${step}_model.pt \
-    --eval_data ${data_dir}/fusion_retrieved_${mode}.jsonl \
+    --fusion_retr_model ./output/train_syt_${dataset}_${expr}_${sql_expr}_tagged/epoc_${epoch}_step_${step}_model.pt \
+    --eval_data ${data_dir}/fusion_retrieved_test_tagged.jsonl \
     --n_context 100 \
     --per_gpu_batch_size 1 \
     --cuda 0 \
-    --name syt_${mode}_${dataset}_${expr}_step_${step} \
+    --name syt_${mode}_${dataset}_${expr}_${sql_expr}_step_${step}_tagged \
     --checkpoint_dir output \
