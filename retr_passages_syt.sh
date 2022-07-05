cf4f3ed05761a02389414a9b563419f0a99e8cf3
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-07-05 16:17:13 -0500

    update ret script

--- retr_passages_syt.sh
+++ retr_passages_syt.sh
@@ -1,21 +1,16 @@
-if [ "$#" -ne 4 ]; then
-  echo "Usage: ./retr_passages_syt.sh <dataset> <experiment> <passage file> <sql_expr>"
-  exit
-fi
-
-dataset=$1
-expr=$2
-file_name=$3
-sql_expr=$4
+mode=$1
+dataset=nq_tables
+expr=rel_graph
+file_name=passages.jsonl
+sql_expr=sql_data
 sql_data_dir=~/code/open_table_discovery/table2question/dataset/${dataset}/${sql_expr}
-out_dir=${sql_data_dir}/${expr}
 
 python ./passage_ondisk_retrieval.py \
-    --model_path ./pretrained_models/tqa_retriever \
+    --model_path ~/code/models/tqa_retriever \
     --index_file ./data/on_disk_index_${dataset}_${expr}/populated.index \
     --passage_file ./data/on_disk_index_${dataset}_${expr}/${file_name} \
-    --data ${sql_data_dir}/fusion_query.jsonl \
-    --output_path ${out_dir}/fusion_retrieved.jsonl \
+    --data ${sql_data_dir}/${mode}/fusion_query.jsonl \
+    --output_path ${sql_data_dir}/${mode}/${expr}/fusion_retrieved.jsonl \
     --n-docs 100 \
-    --min_tables 10 \
-    --max_retr 10000
+    --min_tables 5 \
+    --max_retr 1000
