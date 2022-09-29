60ce868942985eeaae303566ddc6379d41bc8e1b
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-09-28 23:25:54 -0500

    update msg

--- finetune_table_retr.py
+++ finetune_table_retr.py
@@ -713,7 +713,7 @@ if __name__ == "__main__":
     args = options.parse()
     args.sql_batch_no = 0
     msg_info = main(args)
-    if msg_info.get('state', False):
+    if not msg_info.get('state', False):
         print(msg_info['msg'])
 
 
