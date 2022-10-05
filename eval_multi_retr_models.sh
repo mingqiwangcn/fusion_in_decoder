02b920a8b8cb098a20d33ed9d40ed8c754fede45
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-10-05 15:42:17 -0500

    update log_prob and batch size

--- eval_multi_retr_models.sh
+++ eval_multi_retr_models.sh
@@ -12,7 +12,7 @@ python ./finetune_table_retr.py \
     --model_path ~/code/models/tqa_reader_base \
     --eval_data ~/code/open_table_discovery/table2question/dataset/${dataset}/sql_data/train_0/rel_graph/data_parts/${part_no}.jsonl \
     --n_context 100 \
-    --per_gpu_eval_batch_size 6 \
+    --per_gpu_eval_batch_size 4 \
     --cuda 0 \
     --name ${dataset}_multi_models_${part_no} \
     --checkpoint_dir output \
