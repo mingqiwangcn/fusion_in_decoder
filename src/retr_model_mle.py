76b505163410bf5dcaee71db7efa38061d1062b0
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-10-07 23:05:13 -0500

    update mle para

--- src/retr_model_mle.py
+++ src/retr_model_mle.py
@@ -10,6 +10,6 @@ class RetrModelMLE(FusionRetrModelBase):
         return CustomLinear(in_features, out_features) 
     
     def sample_forward(self, batch_data, fusion_scores, fusion_states, passage_masks,
-                sample=False, calculate_log_probs=False, opts=None):
+                sample=False, calculate_log_probs=False, opts=None, num_samples=None):
         return self(batch_data, fusion_scores, fusion_states, passage_masks,
                 sample=sample, calculate_log_probs=calculate_log_probs, opts=opts)
